65d7162644391836fe54626753133ef3c34bc49b
zaitsevmaksym <zaytsev.maks@gmail.com>
2022-04-05 11:52:14 +0300

    clone repo from web
    clone repo from web

---
+++ bd/company.sql
@@ -0,0 +1,2 @@
+INSERT INTO `company` (`id`, `name`, `position`, `region`, `age`, `begin`) VALUES (1, 'pupkin', 'buhgalter', 'ukr', 21, '2021-08-19');
+INSERT INTO `company` (`id`, `name`, `position`, `region`, `age`, `begin`) VALUES (2, 'dudkin', 'florist', 'kyiv', 22, '2021-08-19');
